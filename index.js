96639f24136ba2a5279b37bf50107a92ee16a6ee
Nigel Phang <nigelprankster5@gmail.com>
2026-05-15 22:05:52 +0800

    this is 10.06 minutes worth of bro_code covering javascript basics

---
+++ index.js
@@ -0,0 +1,14 @@
+//console.log(`Hello`);
+//console.log(`I like pizza`);
+
+//window.alert(`this is an alert !`);
+//window.alert(`i like pizza`);
+
+// This is a comment
+
+/* 
+    This 
+    is 
+    a 
+    comment
+*/
