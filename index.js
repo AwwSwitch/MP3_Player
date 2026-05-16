dafa0a8833584ac988cfcb3a508f5459c70d06f2
Nigel Phang <nigelprankster5@gmail.com>
2026-05-16 12:50:32 +0800

    Final code for JavaScript tutorial for Beginners

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 //window.alert(`this is an alert !`);
 //window.alert(`i like pizza`);
 
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I Like pizza`;
+
 // This is a comment
 
 /* 
