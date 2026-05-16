cb7801594c9e35e1ccb048a61b36de3b3839e55b
Nigel Phang <nigelprankster5@gmail.com>
2026-05-16 15:14:36 +0800

    chapter 2 variable completed !

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+//chapter 1 (into to javascript)
 //console.log(`Hello`);
 //console.log(`I like pizza`);
 
 //window.alert(`this is an alert !`);
 //window.alert(`i like pizza`);
 
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I Like pizza`;
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myP").textContent = `I Like pizza`;
 
 // This is a comment
 
-/* 
-    This 
-    is 
-    a 
-    comment
-*/
+// variable = A container that stores a value
+// declaration and assignment
+
+
---
+++ variable.js
@@ -0,0 +1,37 @@
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+let firstName = "i hate niggers";
+
+let online = true;
+
+
+console.log(typeof online);
+console.log(`bro is online ${online}`);
+console.log(typeof gpa);
+console.log(`you are ${age} yers old`);
+console.log(`the price is ${price}`);
+console.log(`your gpa is ${gpa}`);
+console.log(typeof firstName);
+console.log(`my name is ${firstName}`);
+
+// boolean
+// index.js:26 bro is online true
+// index.js:27 number
+// index.js:28 you are 25 yers old
+// index.js:29 the price is 10.99
+// index.js:30 your gpa is 2.1
+// index.js:31 string
+// index.js:32 my name is i hate niggers
+
+let fullname = "nigel phang";
+let age = 25;
+let student = true;
+
+document.getElementById("p1").textContent = `my full name is ${fullname}`;
+document.getElementById("p2").textContent = `i am ${age} years old`;
+document.getElementById("p3").textContent = `i am ${student} student`;
+
+// output would display 3 paragraph corresponding to the following no biggie
+
